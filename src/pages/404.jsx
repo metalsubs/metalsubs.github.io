8e4dc52a2e294b68c8bcc43f0a6484a6f968213c
Sergio Flores <byoigres@gmail.com>
2020-11-21 12:03:30 -0700

    Added 404 style

--- src/pages/404.jsx
+++ src/pages/404.jsx
@@ -1,12 +1,46 @@
 import React, { Fragment } from "react"
+import styled, { keyframes } from 'styled-components'
 
 import SEO from "../components/SEO"
+
+const karaoke = keyframes`
+  from { width: 0 }
+  to { width: 100% }
+`
+
+const P = styled.p`
+  font-weight: bold;
+  font-size: 2rem;
+  font-family: "Holtwood One SC";
+  text-align: center;
+`
+
+const Words = styled.span`
+  position: relative;
+  white-space: nowrap;
+  color: lightblue;
+  text-shadow: 0 0 3px rgba(0, 0, 0, 1);
+
+  &::after {
+    content: attr(data-text);
+    position: absolute;
+    left: 0;
+    top: 0.5rem;
+    color: red;
+    overflow: hidden;
+    width: 40%;
+    animation: ${karaoke} 5s infinite linear;
+    text-shadow: 0 0 3px rgba(255, 255, 255, 1);
+  }
+`
+
 
 const NotFoundPage = () => (
   <Fragment>
     <SEO title="404: Not found" />
-    <h1>NOT FOUND</h1>
-    <p>You just hit a route that doesn&#39;t exist... the sadness.</p>
+    <P>
+      <Words data-text="Page not found">Page not found</Words>
+    </P>
   </Fragment>
 )
 
